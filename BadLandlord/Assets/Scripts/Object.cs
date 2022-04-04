a625067e0a2d1adccdae63330d8060941cb7585c
eendo01 <elizabeth.endo@tufts.edu>
2022-04-04 00:17:50 -0400

    object simple break and fix

--- BadLandlord/Assets/Scripts/Object.cs
+++ BadLandlord/Assets/Scripts/Object.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class Object : MonoBehaviour
 {
     public bool active;
+    public bool broken;
     public SpriteRenderer spriteRenderer;
     public Sprite[] spriteArray = new Sprite[3];
     const int PERFECT = 0, FINE = 1, BROKEN = 2;
@@ -14,45 +15,74 @@ public class Object : MonoBehaviour
     // times for perfect, fine
     public int[] BREAKTIMES = new int[2];
 
-    Dropdown m_Dropdown;
+    //Dropdown m_Dropdown;
     //public Text m_Text; 
 
     // Start is called before the first frame update
     void Start()
     {
         active = true;
+        broken = false;
         curState = PERFECT;
         spriteRenderer.sprite = spriteArray[curState];
 
-        m_Dropdown = GetComponentInChildren<Dropdown>();
-        //Add listener for when the value of the Dropdown changes, to take action
-        m_Dropdown.onValueChanged.AddListener(delegate
-        {
-            DropdownValueChanged(m_Dropdown);
-        });
+        StartCoroutine(PerfectWaitBreak());
+
+        //m_Dropdown = GetComponentInChildren<Dropdown>();
+        ////Add listener for when the value of the Dropdown changes, to take action
+        //m_Dropdown.onValueChanged.AddListener(delegate
+        //{
+        //    DropdownValueChanged(m_Dropdown);
+        //});
     }
 
-
-    //Ouput the new value of the Dropdown into Text
-    void DropdownValueChanged(Dropdown change)
+    // Update is called once per frame
+    void Update()
     {
-        Debug.Log(m_Dropdown.value);
+        if (Input.GetKeyDown("space") && broken)
+        {
+            ChangeObjectState(PERFECT);
+
+            StartCoroutine(PerfectWaitBreak());
+        } 
     }
 
-        // Update is called once per frame
-        void Update()
+    IEnumerator PerfectWaitBreak()
     {
-        if (Input.GetKeyDown("space"))
-        {
-            Debug.Log("space");
-            m_Dropdown.Show();
-        }
+        yield return new WaitForSeconds(BREAKTIMES[PERFECT]);
+        ChangeObjectState(BROKEN);
     }
+
+    //private void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        Debug.Log("player trigger");
+    //        if (broken)
+    //        {
+    //            Debug.Log("should change idk");
+    //            ChangeObjectState(PERFECT);
+    //        }
+    //    }
+    //}
 
     // Changes object state (perfect, fine, broken)
     void ChangeObjectState(int newState)
     {
         curState = newState;
+
+        // update sprite to correct image
         spriteRenderer.sprite = spriteArray[curState];
+
+        // update broken bool to correct value
+        broken = newState == BROKEN ? true : false;
     }
+
+    //Ouput the new value of the Dropdown into Text
+    //void DropdownValueChanged(Dropdown change)
+    //{
+    //    Debug.Log(m_Dropdown.value);
+    //}
+
+
 }
