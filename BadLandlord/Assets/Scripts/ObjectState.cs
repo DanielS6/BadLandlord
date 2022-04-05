bdd5572a41f6833f34a6dd2c9acbccca7d23d01a
eendo01 <60273291+eendo01@users.noreply.github.com>
2022-04-04 23:22:52 -0400

    interactive object and fix (#8)

---
+++ BadLandlord/Assets/Scripts/ObjectState.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectState : MonoBehaviour
+{
+    public bool active;
+    public bool broken;
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] spriteArray = new Sprite[3];
+    const int PERFECT = 0, FINE = 1, BROKEN = 2;
+    public int curState;
+    List<string> dropOptions =
+        new List<string> { "What will you do?", "Buy new - $20", "Quick Fix - $10", "Ignore - $0" };
+    public Dropdown objectMenu;
+
+    // times for perfect, fine
+    public int[] BREAKTIMES = new int[2];
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        active = true;
+        broken = false;
+        curState = PERFECT;
+        spriteRenderer.sprite = spriteArray[curState];
+
+        objectMenu.Hide();
+        objectMenu.ClearOptions();
+
+        StartCoroutine(WaitBreak());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator WaitBreak()
+    {
+        yield return new WaitForSeconds(BREAKTIMES[curState]);
+        ChangeObjectState(BROKEN);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (broken)
+            {
+                objectMenu.AddOptions(dropOptions);
+                objectMenu.Show();
+            }
+        }
+    }
+
+    public void Fix()
+    {
+        ChangeObjectState(objectMenu.value - 1);
+        objectMenu.ClearOptions();
+    }
+
+    // Changes object state (perfect, fine, broken)
+    private void ChangeObjectState(int newState)
+    {
+        curState = newState;
+
+        // update sprite to correct image
+        spriteRenderer.sprite = spriteArray[curState];
+
+        // update broken bool to correct value
+        broken = newState == BROKEN ? true : false;
+
+        if (newState == PERFECT)
+        {
+            StartCoroutine(WaitBreak());
+        }
+        else if (newState == FINE)
+        {
+            StartCoroutine(WaitBreak());
+        }
+    }
+
+}
