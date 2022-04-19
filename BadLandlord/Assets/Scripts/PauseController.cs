1f355b077801c6f646ade83eb84e4efefea48716
DanielS6 <Daniel@nevet.com>
2022-04-18 22:11:31 -0400

    [Pause] use escape key to pause or resume
    When the game is running, the first frame that the escape key is pressed, the
    game will pause, and if pressed again, it will resume. This replaces the old
    code in GameHandler.cs that was calling Application.Quit(), which didn't
    appear to be working when testing. GameHandler.cs is deleted entirely since
    that was the only code it included, and the game handler prefab is updated
    accordingly.

--- BadLandlord/Assets/Scripts/PauseController.cs
+++ BadLandlord/Assets/Scripts/PauseController.cs
@@ -10,35 +10,58 @@ public class PauseController : MonoBehaviour {
     public GameObject pauseButton;
     public GameObject pauseMenu;
 
+    // State to know what to do when the escape key is pressed, either pause
+    // or resume
+    private bool currentlyPaused;
+
     void Start() {
         // Pause menu starts hidden
+        currentlyPaused = false;
         pauseMenu.SetActive(false);
+    }
+
+    void Update() {
+        if (Input.GetKeyDown("escape")) {
+            // Use GetKeyDown() instead of GetKey() so that when the key is
+            // held for multiple frames (which is almost guaranteed to happen)
+            // we only toggle the state once
+            if (currentlyPaused) {
+                ResumeGame();
+            } else {
+                PauseGame();
+            }
+        }
     }
 
     public void PauseGame() {
         Time.timeScale = 0f;
         pauseMenu.SetActive(true);
         pauseButton.SetActive(false);
+        currentlyPaused = true;
     }
 
     public void ResumeGame() {
         Time.timeScale = 1f;
         pauseMenu.SetActive(false);
         pauseButton.SetActive(true);
+        currentlyPaused = false;
     }
 
     public void RestartLevel() {
         // Named restart level and made scene-agnostic in case we later switch
         // to having multiple game scenes
-        // Time should run again for the resumed scene
+        // Time should run again for the resumed scene, and no longer paused
         Time.timeScale = 1f;
+        currentlyPaused = false;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void QuitGame() {
         // Ensure time is running again for anything else in the project,
-        // doesn't matter for the current scene since we are leaving
+        // doesn't matter for the current scene since we are leaving, same
+        // with pause state
         Time.timeScale = 1f;
+        currentlyPaused = false;
         SceneManager.LoadScene("IntroScene");
     }
 }
