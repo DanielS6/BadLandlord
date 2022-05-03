d2eab3c8ed104affd20e261baf83b8e3ff1db1f0
trish234 <trishacox@gmail.com>
2022-05-02 22:07:00 -0400

    add notification bar

---
+++ BadLandlord/Assets/Scripts/NotificationBar.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotificationBar : MonoBehaviour
+{
+    public GameObject notificationText;
+
+    void Start()
+    {
+        notificationText.GetComponent<Text>().text = "";
+    }
+    public void display(string textToDisplay){
+        Text curMessage = notificationText.GetComponent<Text>();
+        curMessage.text = textToDisplay;
+
+    }
+}
