4c1271c76f54950b0d95f3a3615f78a58f8eb3f1
DanielS6 <Daniel@nevet.com>
2022-05-03 09:38:47 -0400

    [LandlordElevator] avoid errors for unset destinations
    When there is no up or down destination set, which occurs for the elevator at
    the top floor and bottom floor, don't report errors about the destinations not
    being set, by checking if they are set before using them.

--- BadLandlord/Assets/Scripts/LandlordElevator.cs
+++ BadLandlord/Assets/Scripts/LandlordElevator.cs
@@ -7,25 +7,29 @@ public class LandlordElevator : MonoBehaviour
     private GameObject currElevator;
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E)) //up for now
-        {
-            if (currElevator != null)
-            {
-                transform.position = currElevator.GetComponent<Elevator>().GetDestinationUp().position;
+    void Update() {
+        // Can't do anything if the current elevator isn't set
+        if (currElevator == null) {
+            return;
+        }
+        Elevator elevatorComponent = currElevator.GetComponent<Elevator>();
+        // E - up for now
+        if (Input.GetKeyDown(KeyCode.E)) {
+            // Don't break if there is no up destination, e.g. for the top floor
+            if ( elevatorComponent.GetDestinationUp() ) {
+                transform.position = elevatorComponent.GetDestinationUp().position;
             }
         }
-        
-        if (Input.GetKeyDown(KeyCode.W)) // down for now
-        {
-            if (currElevator != null)
-            {
-                transform.position = currElevator.GetComponent<Elevator>().GetDestinationDown().position;
+
+        // W - down for now
+        if (Input.GetKeyDown(KeyCode.W)) {
+            // Don't break if there is no down destination, e.g. for the bottom floor
+            if ( elevatorComponent.GetDestinationDown() ) {
+                transform.position = elevatorComponent.GetDestinationDown().position;
             }
         }
     }
-    
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         // Landlord interaction, give options for fixing
@@ -33,9 +37,9 @@ public class LandlordElevator : MonoBehaviour
         {
             currElevator = other.gameObject;
             //anim.SetBool("get_on", true);
-        } 
+        }
     }
-    
+
     private void OnTriggerExit2D(Collider2D other)
     {
         // hide interact prompt
