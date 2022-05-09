164704c79dabb856ec2635b37e6e551460da5729
DanielS6 <Daniel@nevet.com>
2022-05-09 08:58:58 -0400

    [StoryTimer] cleanup and remove debugging code
    Tweak whitespace, remove logging for before and after the timer delay

--- BadLandlord/Assets/Scripts/Backstory Scripts/StoryTimer.cs
+++ BadLandlord/Assets/Scripts/Backstory Scripts/StoryTimer.cs
@@ -4,23 +4,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StoryTimer : MonoBehaviour
-{
+public class StoryTimer : MonoBehaviour {
     public GameObject objectToActivate;
     public int seconds = 15;
     public bool on = true;
 
-    private void Start()
-    {
+    private void Start() {
         StartCoroutine(ActivationRoutine());
     }
 
-    private IEnumerator ActivationRoutine()
-    {        
+    private IEnumerator ActivationRoutine() {
         objectToActivate.SetActive(on);
-        Debug.Log("about to wait");
         yield return new WaitForSeconds(seconds);
-        Debug.Log("done waiting I");
         objectToActivate.SetActive(!on);
     }
 }
